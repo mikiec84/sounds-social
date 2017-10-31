cd2a6b391b5367e2b6cfc66402642f621ea960ce
Matteo De Micheli <matteodem@protonmail.com>
2017-10-31 14:47:36 +0100

    Add simple test for api

---
+++ code/api/imports/lib/resolvePromiseForCallback.test.js
@@ -0,0 +1,26 @@
+import { chai } from 'meteor/practicalmeteor:chai'
+import { resolvePromiseForCallback } from './resolvePromiseForCallback'
+
+describe('resolvePromiseForCallback', function () {
+  it('validly resolves', function (done) {
+    const resolve = () => {
+      done()
+    }
+    const reject = () => {
+      throw new Error('should not be called')
+    }
+
+    resolvePromiseForCallback(resolve, reject)(null, true)
+  })
+
+  it('validly rejects', function (done) {
+    const resolve = () => {
+      throw new Error('should not be called')
+    }
+    const reject = () => {
+      done()
+    }
+
+    resolvePromiseForCallback(resolve, reject)(new Error('what the heck'))
+  })
+})
