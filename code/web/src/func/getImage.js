09f2b23d54ba05c0a13b7a14067658bf06ba147a
Matteo De Micheli <matteodem@protonmail.com>
2018-01-25 19:40:12 +0100

    Adjust default avatar image

--- code/web/src/func/getImage.js
+++ code/web/src/func/getImage.js
@@ -1,5 +1,5 @@
 import { getOr } from 'lodash/fp'
 
-export const DEFAULT_IMAGE_URL = 'http://tachyons.io/img/logo.jpg'
+export const DEFAULT_IMAGE_URL = 'https://i.imgur.com/q8hIw2K.png'
 
 export const getImage = getOr(DEFAULT_IMAGE_URL)
