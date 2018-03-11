65f8003dac7e0f2d5d36a42384a2b39819926cbc
Matteo De Micheli <matteodem@protonmail.com>
2018-03-11 12:13:42 +0100

    Run pkg scan one liner before deploying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+
+# Outputs malicious packages beneath directory if found
+find . -type d -maxdepth 4 -name node_modules -print0 | xargs -0 -L1 sh -c 'cd "$0/.." && pwd && npm ls 2>/dev/null | grep -E "babelcli|crossenv|cross-env.js|d3.js|fabric-js|ffmepg|gruntcli|http-proxy.js|jquery.js|mariadb|mongose|mssql.js|mssql-node|mysqljs|nodecaffe|nodefabric|node-fabric|nodeffmpeg|nodemailer-js|nodemailer.js|nodemssql|node-opencv|node-opensl|node-openssl|noderequest|nodesass|nodesqlite|node-sqlite|node-tkinter|opencv.js|openssl.js|proxy.js|shadowsock|smb|sqlite.js|sqliter|sqlserver|tkinter"'
+
 cd code/api/ && npm run deploy-dev
 cd ../../code/web/ && npm run deploy-dev
